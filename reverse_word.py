e63e1a0708bd762e4b9f7e7191682c39cd355a91
GMI <gmi@Patricks-MacBook-Pro.local>
2020-03-03 18:12:20 +0100

    updated reverse_word imports

--- reverse_word.py
+++ reverse_word.py
@@ -1,11 +1,10 @@
-from python_test_framework import codewars_test as test
+import codewars_test as test
 
 def reverse_words(text):
     words = text.split()
     for word in words:
         print(word.capitalize())
     return text
-
 
 test.assert_equals(reverse_words('The quick brown fox jumps over the lazy dog.'),
                    'ehT kciuq nworb xof spmuj revo eht yzal .god')
