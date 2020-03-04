eb57e4f2a8fcd08a1c0c12a69bf30c9c9cbac472
GMI <gmi@Patricks-MacBook-Pro.local>
2020-03-04 14:57:21 +0100

    changed file watcher to compare files by lastModifiedDate

--- file_watcher.py
+++ file_watcher.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import os
 import sys
 import getopt
@@ -7,30 +8,34 @@ import argparse
 from threading import Timer
 
 print(str(sys.argv))
-old_content = ""
+
+last_modified = 0
 
 
 def script():
     parser = argparse.ArgumentParser(prog='watcher')
-    parser.add_argument('--file', help='Provide path to file')
+    parser.add_argument('-f', '--file', help='Provide path to file')
     file_path = parser.parse_args().file
     cwd = os.path.join(os.getcwd(), file_path)
+    last_modified = os.stat(cwd).st_mtime
     file = open(cwd, 'r')
     name = file.name
 
     def watch_and_rerun(file):
-        global old_content
-        # print('Reading {0} - {1}'.format(name, cwd))
-        content = file.read()
-        # print('Comparing {0}'.format(name))
-        if old_content != content:
-            old_content = content
-            print('Running script {0}'.format(name))
-            os.system('python {0}'.format(cwd))
-
+        global last_modified
+        actual_modified = os.stat(cwd).st_mtime
+        if last_modified != actual_modified:
+            try:
+                os.system('python {0}'.format(cwd))
+                print('After script {0} - {1}'.format(name, datetime.now()))
+            except:
+                print('Error in script {0} - {1}'.format(name, datetime.now()))
+            print('Running script {0} - last_modified: {1} actual_modified: {2}'.format(
+                name, last_modified, actual_modified))
+            last_modified = actual_modified
     while 1:
-        watch_and_rerun(file)
         time.sleep(0.5)
+        watch_and_rerun(file)
 
 
 try:
@@ -38,5 +43,3 @@ try:
 except KeyboardInterrupt:
     print >> sys.stderr, '\nExiting by user request.\n'
     sys.exit(0)
-except:
-    print('error {0}'.format(sys.exc_info()[0]))
